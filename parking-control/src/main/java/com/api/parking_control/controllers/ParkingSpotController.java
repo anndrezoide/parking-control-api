9dffd61dfe2499d26229fae8075ea57349316e3c
André Gomes <dhegomeez@gmail.com>
2025-03-24 21:03:15 -0300

    Feat: Implementeção do metodo saveParkingSpot no controller

--- parking-control/src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -1,5 +1,37 @@
 package com.api.parking_control.controllers;
 
-public class ParkingSpotController {
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parking_control.dtos.ParkingSpotDto;
+import com.api.parking_control.models.ParkingSpotModel;
+import com.api.parking_control.services.ParkingSpotService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600) //permite que seja acessado de qualquer fonte.
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+	
+	@Autowired
+	private ParkingSpotService parkingSpotService;
+
+	@PostMapping
+	public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+		var parkingSpotModel = new ParkingSpotModel();
+		BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel); //transforma um dto em um model
+		parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+		return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+	}
 }
