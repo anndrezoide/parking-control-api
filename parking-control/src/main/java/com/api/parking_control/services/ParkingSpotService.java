d1700ba641b70bf94c4c5a694f784528b5c2451f
André Gomes <dhegomeez@gmail.com>
2025-03-25 21:03:13 -0300

    Feat: adicionada a funcionalidade de listagem por ID e de delete

--- parking-control/src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -1,5 +1,9 @@
 package com.api.parking_control.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +33,20 @@ public class ParkingSpotService {
 	public boolean existsByApartamentAndBlock(String apartament, String block) {
 		return parkingSpotRepository.existsByApartamentAndBlock(apartament, block);
 	}
+
+	public List<ParkingSpotModel> findAll() {
+		return parkingSpotRepository.findAll();
+	}
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		return parkingSpotRepository.findById(id);
+	}
+
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+	}
+
 	
 	
 	
